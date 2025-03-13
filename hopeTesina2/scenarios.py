4e982f397a48ff97e90c5364029d55af64bf628b
Giuseppe Alfieri <150775639+giusalfieri@users.noreply.github.com>
2025-03-13 17:00:39 +0100

    Update scenarios.py

--- hopeTesina2/scenarios.py
+++ hopeTesina2/scenarios.py
@@ -1,4 +1,16 @@
 def params_selection(name, values):
+    """
+    Allows the user to interactively select a value from a provided list of choices. The function continuously prompts 
+    the user until a valid selection is made or the user chooses to quit. The function returns the selected value 
+    or `None` if the user opts to quit.
+
+    :param name: The name or label of the category for which a choice is being made, used in display messages.
+    :type name: str
+    :param values: List of possible values from which the user can make a selection.
+    :type values: list
+    :return: The selected value if a valid selection is made, or `None` if the user chooses to quit.
+    :rtype: str | None
+    """
     while True:
         print(f"\nChoose a value for {name}:")
         for i, val in enumerate(values):
